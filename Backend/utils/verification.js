bbddee59319fce164a9aec7194522048717fa1fd
noudas <noudas1@gmail.com>
2025-03-24 12:44:37 +0200

    Creating two factor autentication

--- Backend/utils/verification.js
+++ Backend/utils/verification.js
@@ -7,6 +7,14 @@ export function checkEmailVerified(req, res, next) {
     next();
 }
 
+export function checkPhoneVerified(req, res, next) {
+    if (req.user.is_phone_verified != 1) {
+        return res.status(403).json({ error: 'Phone not verified.' });
+    }
+    next();
+}
+
+
 export function checkAdmin(req, res, next) {
     if (req.user.authorization_level !== 'admin') {
         return res.status(403).json({ error: 'Admin access required.' });
