f3e939961cd2f8f6edca9dd4c49d6ab91bdc1e03
noudas <noudas1@gmail.com>
2025-03-26 13:58:07 +0200

    Making the constants

---
+++ FrontEnd/app/constants/Colors.ts
@@ -0,0 +1,19 @@
+const darkGreen = '#39B54A';
+const lightGreen = '#5BD867';
+const red = '#E81A4B';
+const pink = '#F27D98';
+const white = '#F0F7F7';
+const black = '#101010';
+const gray = '#2C2C2C';
+const lightGray ='#C4C4C40';
+
+export const Colors = {
+    primary: darkGreen,
+    secondary: lightGreen,
+    danger: red,
+    accent: pink,
+    background: white,
+    text: black,
+    muted: gray,
+    lightText: lightGray,
+}
